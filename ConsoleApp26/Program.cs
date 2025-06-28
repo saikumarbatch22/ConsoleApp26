3fc6e35e5e9167010c995404dfca93f7d47c653c
kumar6646 <sai.penusoft@gmail.com>
2025-06-28 10:49:15 +0530

    Add project files.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    /*
+     * Switch: It is also a conditional branching
+     * It has case and break statements and followed 
+     * by default if none of the cases executes then it will go
+     * to default case.
+     * 
+     * syntax:
+     * switch(expression)
+     * {
+     * case '':
+     *  statements;
+     *  break;
+     *  case '':
+     *  statements;
+     *  break;
+     *  case '':
+     *  statements;
+     *  break;
+     *  ....
+     *  
+     *  default:
+     *  statements;
+     *  break;
+     *  
+     *  break is jump statement but in switch case it is used to 
+     *  terminate the given statement.
+     *  
+     *  switch statement directly switches to the particular statement 
+     *  rather than checking each and every statement.
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character");
+            char s=Convert.ToChar(Console.ReadLine());
+            switch(s)
+            {
+                case 'a':
+                    Console.WriteLine("Entered character is a vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("Entered character is a vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("Entered character is a vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("Entered character is a vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("Entered character is a vowel");
+                    break;
+                default:
+                    Console.WriteLine("Enterd character is a consonant");
+                    break;
+            }
+
+        }
+    }
+}
